fca79135023de5c7f8f201f181ea36d9018a8c02
Veyefill <nojusklimov@gmail.com>
2024-05-07 03:33:06 +0300

    Exercise done

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [guess, setGuess] = useState('');
+  const [times, setTimes] = useState(0);
+  const [title, setTitle] = useState("Guess a number between 1-100");
+
+  useEffect(() => {
+    setNumber(Math.floor(Math.random() * 100) + 1);
+  }, []);
+
+  const MakeGuess = () => {
+    const intGuess = parseInt(guess);
+    setTimes(times + 1);
+    if (intGuess === number) {
+      Alert.alert("You guessed the number in " + times + " guesses");
+    } else if (intGuess > number) {
+      setTitle("Your guess " + intGuess + " is too high");
+    } else if (intGuess < number) {
+      setTitle("Your guess " + intGuess + " is too low")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontWeight: "bold" }}>{title}</Text>
+      <TextInput style={{ width: 300, borderColor: 'black', borderWidth: 1 }} onChangeText={guess => setGuess(guess)} value={guess} keyboardType="numeric"></TextInput>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={MakeGuess} title="Make a Guess"></Button>
+      </View>
+    </View >
   );
 }
 
